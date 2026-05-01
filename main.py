ed15f3eb23ce65325c0dd84e345587e94e5e9825
Henrik Tømmervik <he-vt@hotmail.com>
2026-05-01 09:07:13 +0200

    Create main.py file with bot implementation

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Create bot instance
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
